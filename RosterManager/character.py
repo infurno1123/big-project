ccb682a5e0f6279558330b725e11c9344e60613a
Danny <dgami001@ucr.edu>
2026-06-08 20:30:47 -0700

    added introductory functions for the file

--- RosterManager/character.py
+++ RosterManager/character.py
@@ -1,1 +1,23 @@
 #This defines the character "class", compiling all the information of said character.
+class Character:
+    def __init__(self, name, game, level, rarity, element, role):
+        self.name = name
+        self.game = game
+        self.level = level
+        self.rarity = rarity
+        self.element = element
+        self.role = role
+
+    def display(self):
+            print("-"*30)
+            print("Name:", self.name)
+            print("Game:", self.game)
+            print("Level:", self.level)
+            print("Rarity:", str(self.rarity) + "★")
+            print("Element:", self.element)
+            print("Role:", self.role)
+            print("-"*30)
+
+welt = Character("Welt", "HSR", 80, 5, "Imaginary", "Delay Specialist")
+
+welt.display()
