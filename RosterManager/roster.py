10b351ac517130745a062cb29b325658642338d2
Danny <dgami001@ucr.edu>
2026-06-10 21:07:29 -0700

    added first portion of roster creation.

--- RosterManager/roster.py
+++ RosterManager/roster.py
@@ -1,1 +1,15 @@
 #This is responsible for managing the collection of characters.
+#Note that this file should only have functions/methods that are used by the MAIN file to complete it's given task
+class Roster:
+    #where self would be a character's name
+    def __init__(self):
+        self.characters = []
+
+        
+
+
+#Notation practice below
+#welt = Character("Welt", "HSR", 80, 5, "Imaginary", "Delay Specialist")
+
+#welt.level_up()
+#print(welt.display())
